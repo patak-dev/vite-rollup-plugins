a1a4b51280686190a04a666f0818ab6d3c06427a
Matias Capeletto <matias.capeletto@gmail.com>
2021-01-31 22:28:27 +0100

    feat: rollup-plugin-terser is covered

--- src/components/CommunityPlugins.vue
+++ src/components/CommunityPlugins.vue
@@ -1,11 +1,24 @@
 <template>
   <div class="playground">
-    <PluginOffMainThread />
+    <PluginCard
+      name="@surma/rollup-plugin-off-main-thread"
+      docs="https://github.com/surma/rollup-plugin-off-main-thread#readme"
+      description="Use Rollup with workers and ES6 modules today"
+      status="covered"
+      link="https://vitejs.dev/guide/features.html#web-workers"
+    />
+    <PluginCard
+      name="rollup-plugin-terser"
+      docs="https://github.com/TrySound/rollup-plugin-terser#readme"
+      description="Minify generated es bundle. Uses terser under the hood"
+      status="covered"
+      link="https://vitejs.dev/config/#build-minify"
+    />
   </div>
 </template>
 
 <script setup>
-import PluginOffMainThread from "./community/PluginOffMainThread.vue";
+import PluginCard from './PluginCard.vue'
 </script>
 
 <style scoped>
