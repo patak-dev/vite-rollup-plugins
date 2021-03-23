a01e8c932887c3aebc0e9fcd87957de1744b217b
Jacob Clevenger <jacobrclevenger@gmail.com>
2021-03-23 06:12:31 -0400

    fix: plugin babel should have info instead of demo

--- src/plugins/PluginBabel.ts
+++ src/plugins/PluginBabel.ts
@@ -22,7 +22,7 @@ export default definePlugin({
   status: PluginStatus.Covered,
   category: PluginCategory.Official,
   link: 'https://vitejs.dev/guide/build.html#browser-compatibility',
-  demo() {
+  info() {
     return html`
       Check out <a href="https://github.com/vitejs/vite/tree/main/packages/plugin-legacy" target="_blank" rel="noopener">@vitejs/plugin-legacy</a> for building for production with old browsers support. 
       In case you want to use experimental features, @rollup/plugin-babel is compatible with Vite but it is not recommended because it could slow down the dev server. It is better to use esbuild plugins for these features.
